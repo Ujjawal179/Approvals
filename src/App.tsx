dc41ac3445b0d39a45eefd8e881f244b698a89ca
Ujjawal179 <ujjawal.agrawal179@gmail.com>
2024-08-31 13:20:06 +0530

    bug 3

--- src/App.tsx
+++ src/App.tsx
@@ -65,6 +65,11 @@ export function App() {
               return
             }
 
+            if(newValue.firstName === "All" && newValue.lastName === "Employees") {
+              await loadAllTransactions()
+              return
+            }
+
             await loadTransactionsByEmployee(newValue.id)
           }}
         />
